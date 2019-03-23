5ee233412183317e92e1bdf3cd5bc2ccffae79d9
arvnmarine <arvnmarine@gmail.com>
2019-03-23 01:01:17 -0700

    fix prblems when other indiacator are activated such as wiper, high beam or blinker

--- gen2SteeringWheel_gen3Cluster.py
+++ gen2SteeringWheel_gen3Cluster.py
@@ -51,6 +51,8 @@ def compareData (data, buttonGen2):
 	
 	
 try:
+
+	recent_releaseBtn = False
 	while True:
 		message = bus.recv()	# Wait until a message is received.
 		#print ("Received Message id ", message.arbitration_id)
@@ -63,40 +65,50 @@ try:
 		
 			
 		
-			
+		sent_permission = True	
 		
-		if compareData(message.data, MSG_GEN2_UP_ARROW_STEERINGWHEEL_LEFT):
+		if message.data[4] == 0x10:
 			print ("UP pressed ")
+			recent_releaseBtn = False
 			for i in range(message.dlc ):
 				message.data[i] = MSG_GEN3_UP_ARROW_STEERINGWHEEL_LEFT[i]
-		elif compareData(message.data , MSG_GEN2_DOWN_ARROW_STEERINGWHEEL_LEFT):
+		elif message.data[4] == 0x04:
 			print ("DOWN pressed ")
+			recent_releaseBtn = False
 			for i in range(message.dlc ):
 				message.data[i] = MSG_GEN3_DOWN_ARROW_STEERINGWHEEL_LEFT[i]
-		elif compareData(message.data , MSG_GEN2_LEFT_ARROW_STEERINGWHEEL_LEFT):
+		elif message.data[4] == 0x01:
 			print ("LEFT pressed ")
+			recent_releaseBtn = False
 			for i in range(message.dlc ):
 				message.data[i] = MSG_GEN3_LEFT_ARROW_STEERINGWHEEL_LEFT[i]
-		elif compareData(message.data , MSG_GEN2_RIGHT_ARROW_STEERINGWHEEL_LEFT):
+		elif message.data[4] == 0x40:
 			print ("RIGHT pressed ")
+			recent_releaseBtn = False
 			for i in range(message.dlc ):
 				message.data[i] = MSG_GEN3_RIGHT_ARROW_STEERINGWHEEL_LEFT[i]
-		elif compareData(message.data , MSG_GEN2_VR_BUTTON_STEERINGWHEEL_LEFT):
+		elif message.data[5] == 0x0E:
 			print ("VR pressed ")
+			recent_releaseBtn = False
 			for i in range(message.dlc ):
 				message.data[i] = MSG_GEN3_OK_BUTTON_STEERINGWHEEL_LEFT[i]
-		elif compareData(message.data , MSG_GEN2_RELEASEBUTTON_STEERINGWHEEL_LEFT):
+		elif (message.data[4] == 0x00) and (message.data[5] == 0x0C):
 			# print ("Released button")
-			for i in range(message.dlc ):
-				message.data[i] = MSG_GEN3_RELEASEBUTTON_STEERINGWHEEL_LEFT[i]
+			if recent_releaseBtn == False :
+				for i in range(message.dlc ):
+					recent_releaseBtn = True
+					message.data[i] = MSG_GEN3_RELEASEBUTTON_STEERINGWHEEL_LEFT[i]
+			else:
+				sent_permission = False
 		else:
 			#print ("Unknown message")
 			continue
 	
 		message.arbitration_id = GEN3_PID_STEERINGWHEEL
 		
-		bus.send(message)
-		time.sleep(0.01)
+		if sent_permission == True :
+			bus.send(message)
+			time.sleep(0.01)
 		
 	
 except KeyboardInterrupt:
